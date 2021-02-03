de7239db75d22636960c9ddc5215a1bdeaa27041
Gabriel Santos <gabrielbarretos2000@outlook.com>
2021-02-02 23:45:43 -0300

    feat(command): custom message added to bukkit frame

--- src/main/java/com/nextplugins/tasks/command/registry/CommandRegistry.java
+++ src/main/java/com/nextplugins/tasks/command/registry/CommandRegistry.java
@@ -2,8 +2,10 @@ package com.nextplugins.tasks.command.registry;
 
 import com.nextplugins.tasks.NextTasks;
 import com.nextplugins.tasks.command.TaskCommand;
+import com.nextplugins.tasks.configuration.GeneralConfiguration;
 import lombok.Data;
 import me.saiintbrisson.bukkit.command.BukkitFrame;
+import me.saiintbrisson.minecraft.command.message.MessageType;
 
 @Data(staticConstructor = "of")
 public final class CommandRegistry {
@@ -16,6 +18,8 @@ public final class CommandRegistry {
         bukkitFrame.registerCommands(
                 new TaskCommand(plugin.getTaskManager())
         );
+
+        bukkitFrame.getMessageHolder().setMessage(MessageType.NO_PERMISSION, GeneralConfiguration.get(GeneralConfiguration::noPermission));
 
     }
 
